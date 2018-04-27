248b32a96b657a73e4b6c55ef25f55e26a630169
Danie <schwartz.napoleon@gmail.com>
2018-04-27 20:14:38 +0200

    G

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq.Expressions;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,13 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Blok blok = new Blok();
+            int [] a = {1, 1};
+            int[] f = {0, 0};
+            int[] j = { 0,1};
+            int[] e = {1, 0, 0};
+            int[] w = {1, 0, 1};
+
         }
     }
 }
