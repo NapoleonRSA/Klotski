8faff46ad39ee1d1a551668ceabcd8b4360ce01f
Danie <schwartz.napoleon@gmail.com>
2018-04-29 16:23:13 +0200

    Check for Dup

--- ConsoleApp1/ConsoleApp1/Blok.cs
+++ ConsoleApp1/ConsoleApp1/Blok.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ConsoleApp1
 {
@@ -9,6 +11,7 @@ namespace ConsoleApp1
         public char[] blockBoard = new char[41];
         public Queue<char[]> queue = new Queue<char[]>(new LinkedList<char[]>());
         public Queue <int> piece = new Queue<int>(new LinkedList<int>());
+        public LinkedList<int[]> node_tree = new LinkedList<int[]>();
 
         public void toShowTree()
         {
@@ -37,16 +40,66 @@ namespace ConsoleApp1
                             {
                                 if (s.Equals(c))
                                 {
-                                    foreach (int i in hastableCode.TryGetValue(s))
+                                    foreach (int i in hastableCode.Keys)
                                     {
-                                        
+                                        n[countN] = i;
+                                        charCount++;
                                     }
                                 }
                             }
                         }
                     }
+
+                    int[] nodeNew = new int[] { };
+                    Array.Copy(n, nodeNew, 45);
+                    bool duplicate = false;
+                    if (parent_InitialIndex > 0)
+                    {
+                        duplicate = isDuplicate(n, parent_InitialIndex, iterator);
+                    }
+
+                    if (!duplicate)
+                    {
+                        storeChild(nodeNew);
+                        isSolution(otherBoard);
+                        int index = node_tree.Select((item, inx) => new {item, inx}).First(x => x.item == nodeNew).inx;
+                        indexofParent.ToArray().ElementAt(index);
+                    }
                 }
             }
+            findChild(indexofParent);
+            toShowTree();
+        }
+
+        private bool isDuplicate(int[] node, int parentInitialIndex, IEnumerator<char[]> iterator)
+        {
+            int[] child = new int[] { }; 
+            Array.Copy(node, child, 45);
+            bool dupFound = false;
+        }
+
+        private bool testInBounds(int i)
+        {
+            if (i<8)
+            {
+                return false;
+            }
+
+            if (i > 35)
+            {
+                return false;
+            }
+
+            if (i%7 == 0)
+            {
+                return false;
+            }
+
+            if ((i + 1) % 7 == 0)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
